8d1deb4f963e8fa1bc25b189315b1cc146dbb2fe
shezan7 <bsse1023@iit.du.ac.bd>
2022-02-20 13:03:56 +0600

    1

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+
+const mongoose = require('mongoose')
+
+const bodyParser = require('body-parser')
+
+
+const productRoutes = require('./api/routes/products')
+const orderRoutes = require('./api/routes/orders')
+
+mongoose.connect('mongodb+srv://shezan7:' + process.env.MONGO_ATLAS_PW + '@cluster0.ic9ur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+//' + process.env.MONGO_ATLAS_PW + '
+
+// app.use((req, res, next) => {
+//     res.status(200).json({
+//         message: 'It works!!!'
+//     })
+// })
+
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.use('/products', productRoutes)
+app.use('/orders', orderRoutes)
+
+// app.use((req, res, next) => {
+//     const error = new Error('Not Found!!!')
+//     error.status = 404
+//     next(error)
+// })
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
+
+module.exports = app
