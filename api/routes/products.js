578de03949915431273148369fb4db4901221817
shezan7 <bsse1023@iit.du.ac.bd>
2022-02-21 22:43:10 +0600

    pull then push

--- api/routes/products.js
+++ api/routes/products.js
@@ -12,12 +12,6 @@ router.get('/', (req, res, next) => {
 })
 
 
-//pull-1 
-//pull-2
-//pull-3
-//pull-4
-
-
 router.post("/", (req, res, next) => {
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
