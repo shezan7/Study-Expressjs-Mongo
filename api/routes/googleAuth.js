bd7b3ddef9e18edf8ad6afd7c70562af00a4042f
shezan7 <bsse1023@iit.du.ac.bd>
2022-03-29 13:10:03 +0600

    routes for google signin (prb: '/good' {req.user})

--- api/routes/googleAuth.js
+++ api/routes/googleAuth.js
@@ -3,29 +3,28 @@ const router = express.Router()
 
 const passport = require("passport")
 
-const checkAuth = require('../middleware/check-auth');
-const { checkUser } = require("./../middleware/roleValid")
-
-const googleAuthController = require('../controllers/users');
-
 
 router.get('/', (req, res) => res.send('You are not logged in'))
 router.get('/failed', (req, res) => res.send('Failed to log in!!!'))
-router.get('/good', (req, res) => res.send(`Welcome ${req.user.name.givenName}`))
+router.get('/good', (req, res) => res.send('Welcome Page'))
+// router.get('/good', (req, res) => { console.log("this", `${req}`) })
+// router.get('/good', (req, res) => res.send(`Welcome ${req.user.displayName}`))
 
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
 
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/failed' }),
     function (req, res) {
-        console.log(req.user)
+        console.log(req.user.id)
         // Successful authentication, redirect home.
         res.redirect('/googleAuth/good');
     });
 
 router.get('/logout', (req, res) => {
     req.session = null
-    req.logOut()
-    res.redirect('/')
+    //req.logOut()
+    res.redirect('/googleAuth')
 })
 
 
